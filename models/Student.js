f86e656eaa0d4d5a6809f6fc0b6a5df1daa5fdab
giselle hemsy <ghemsy@gmail.com>
2023-02-21 18:56:47 -0300

    .

--- models/Student.js
+++ models/Student.js
@@ -36,12 +36,12 @@ const StudentSchema = new Schema(
     },
     email:{
         type:String,
+        required: [true, "El email es obligatorio"],
         trim:true,
         unique:[true, "Ya existe el email"]
     },
     cel:{
         type:Number,
-        required: [true, "El tel es obligatorio"],
         trim: true,
         minLength:[6, "No puede tener menos de 6 caracteres"],
         maxLength:[13, "No puede tener mas de 6 caracteres"]
