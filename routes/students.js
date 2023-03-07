d11098bbf6574d9d22b5387c18e63f67d3bc3cc0
Agos Mercado <agosmercado@gmail.com>
2023-03-06 22:54:39 -0300

    solucionando error con verificacion de role en edicion y eliminacion de estudiantes

--- routes/students.js
+++ routes/students.js
@@ -1,15 +1,16 @@
 const {Router} = require ("express");
 const { addStudent, getStudents, deleteStudent, editStudent, getStudentByCourse, getStudentForEmail } = require("../controllers/student");
+const auth = require("../middlewares/auth");
 const verifyRole = require("../middlewares/verifyRole");
 const router = Router();
 
 
 router.get ("/", getStudents);
 router.get ("/course", getStudentByCourse);
-router.get("/:email?", getStudentForEmail);
-router.put ("/", verifyRole, editStudent );
-router.delete ("/:email?", verifyRole, deleteStudent);
-router.post ("/", verifyRole, addStudent);
+router.get("/email/:email?", getStudentForEmail);
+router.put ("/", auth, verifyRole, editStudent );
+router.delete ("/:email?",auth, verifyRole, deleteStudent);
+router.post ("/",auth, verifyRole, addStudent);
 
-
+// router.get("/email/:email?",getuserForEmail)
 module.exports = router;
